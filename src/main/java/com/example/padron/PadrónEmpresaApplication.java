57eb2cfc4ea2b8d2f882f47a56a80c0b3762941c
Damián Berruezo <damianberruezo@gmail.com>
2020-03-26 14:48:14 -0300

    feat(project): first commit

---
+++ src/main/java/com/example/padron/PadrónEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.example.padron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadrónEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadrónEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/padron/PadrónEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.padron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadrónEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
