89e669a019484cae4535fac5f9c8538b882462ba
Damián Berruezo <damianberruezo@gmail.com>
2020-04-02 16:44:01 -0300

    tests(main): add basic test

--- src/test/java/com/example/padron/MainTests.java
+++ src/test/java/com/example/padron/MainTests.java
@@ -1,13 +1,33 @@
 package com.example.padron;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-class MainTests {
+@AutoConfigureMockMvc
+public class MainTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void testTest () throws Exception {
+		mockMvc.perform(get("/person/12345678"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(
+				jsonPath("$.id", equalTo(12345678))
+			)
+		;
 	}
-
 }
